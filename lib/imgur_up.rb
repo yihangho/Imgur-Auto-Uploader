96cd035ad09e1315827201b6fa34702d78a0ccc9
Yihang Ho <hoyihang5@gmail.com>
2015-03-10 09:16:17 +0800

    Make sure logs are written to the correct place.

--- lib/imgur_up.rb
+++ lib/imgur_up.rb
@@ -5,6 +5,13 @@ require 'logger'
 
 module ImgurUp
   def self.logger
-    @logger ||= Logger.new(File.expand_path("~/Library/Logs/com.yihangho.imgur-auto-uploader.log"))
+    @logger ||=
+      if File.directory?(File.expand_path("~/Library/Logs"))
+        Logger.new(File.expand_path("~/Library/Logs/com.yihangho.imgur-auto-uploader.log"))
+      elsif File.directory?("/var/log")
+        Logger.new("/var/log/com.yihangho.imgur-auto-uploader.log")
+      else
+        Logger.new(STDOUT)
+      end
   end
 end
